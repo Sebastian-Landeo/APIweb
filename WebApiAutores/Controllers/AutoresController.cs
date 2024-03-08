f9bf613257053b97a4de36b029ff5dd57a3325cf
Sebastian Landeo <sebastian.landeo@unmsm.edu.pe>
2024-03-07 20:51:37 -0500

    Agregado el controlador de libros

--- WebApiAutores/Controllers/AutoresController.cs
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -37,8 +37,27 @@ namespace WebApiAutores.Controllers
                 return BadRequest("El id del autor no coincide con el id de la URL");
             }
 
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound();
+            }
+
             context.Update(autor);
             await context.SaveChangesAsync();  
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Autores.AnyAsync(x=>x.Id ==id);
+            if(!existe)
+            {
+                return NotFound();
+            }
+            context.Remove(new Autor { Id = id });
+            await context.SaveChangesAsync();
             return Ok();
         }
     }
